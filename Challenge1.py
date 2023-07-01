2d1d8a96b53b15163a02e37a18b1f96c257821f4
Bloodofcrow <boc1209@gmail.com>
2023-07-01 11:15:28 -0500

    Optimización del primer desafío.

--- Challenge1.py
+++ Challenge1.py
@@ -1,22 +1,22 @@
-#Variable global s
+#Variable goblal s
 s = 4
 
-#Función que examina y remueve los números iguales o superiores a la variable global 
-def examinar (number):
-  cadena = str(number)
-  for entry in cadena:
-    if int(entry) >= s: cadena = cadena.replace(str(entry), "")
-  return cadena
+#Función que elimina los número iguales o mayores a s en el arreglo y lo reordena
+def calcular(array):
 
-#Función que recorre el arreglo
-def recorrer (array):
-  newArray = []
-  for number in array:
-    temporal = examinar(number)
-    if temporal != '':
-      newArray.insert(0, int(temporal))
-  return newArray
+    arrayToStr = ' '.join([str(elem) for elem in array])
+
+    for entry in arrayToStr:
+        if entry != ' ' :
+            if int(entry) >= s: arrayToStr = arrayToStr.replace(entry, "")
+
+    arrayToStr = arrayToStr.split(' ')
+
+    nArray = [int(entry) for entry in arrayToStr if entry != '']
+    nArray = nArray[::-1]
+
+    return nArray
 
 #main
 if __name__ == '__main__':
-  print (recorrer([10, 20, 30, 40]))
+    print(calcular([1,24,3,5,6,152]))
