7a77cce5fb0a05733450fa529c552e89cc65a90b
Bloodofcrow <boc1209@gmail.com>
2023-07-03 04:55:42 -0500

    Actualización desafío 3.

--- Challenge3.py
+++ Challenge3.py
@@ -1,9 +1,10 @@
-#Función que evalúa el menor valor que no se puede generar
+#Función que ordena y envía al cálculo 
 def ordenarYCalular(array):
     array.sort()
     posible = 1  
     return(calcular(array, posible))
 
+#Función que encuentra el menor número que no se puede generar
 def calcular(array, posible):
     if array == [] : return posible + 1
     if array[0] > posible: return posible + 1
@@ -12,6 +13,6 @@ def calcular(array, posible):
 
 #main
 if __name__ == '__main__':
-	print(calcular([6, 1, 1, 2, 22]))  
-	print(calcular([1, 1, 1, 1, 1]))
-	print(calcular([1, 5, 1, 1, 1, 10, 15, 20, 100]))
+	print(ordenarYCalcular([6, 1, 1, 2, 22]))  
+	print(ordenarYCalcular([1, 1, 1, 1, 1]))
+	print(ordenarYCalcular([1, 5, 1, 1, 1, 10, 15, 20, 100]))
