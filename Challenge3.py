eab9ca465cb5df29273c45ffb056136262846e6f
Bloodofcrow <boc1209@gmail.com>
2023-07-03 04:39:30 -0500

    Función recursiva desafío 3.

--- Challenge3.py
+++ Challenge3.py
@@ -1,17 +1,18 @@
 #Función que evalúa el menor valor que no se puede generar
-def calcular(array):
+def ordenarYCalular(array):
     array.sort()
     posible = 1  
+    return(calcular(array, posible))
 
-    for index in array:
-        if index > posible:
-            return posible  
-        posible += index
-
-    return posible + 1 
+def calcular(array, posible):
+    if array == [] : return posible + 1
+    if array[0] > posible:
+            return posible + 1
+    posible += index
+    return(calcular(array[1:], posible))
 
 #main
 if __name__ == '__main__':
 	print(calcular([6, 1, 1, 2, 22]))  
-	print(calcular([1, 1, 1, 1, 1, 2]))
+	print(calcular([1, 1, 1, 1, 1]))
 	print(calcular([1, 5, 1, 1, 1, 10, 15, 20, 100]))
